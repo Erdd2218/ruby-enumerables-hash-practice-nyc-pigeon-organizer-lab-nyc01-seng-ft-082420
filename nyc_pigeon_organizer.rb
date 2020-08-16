e82fa5e61e18c4540ee2f6f8d8a7913cd8247477
Andre Pierre <andre.laurence94@gmail.com>
2020-08-16 04:12:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,20 @@
 def nyc_pigeon_organizer(data)
-  pigeon_name = {} 
-  
-  data.each do |k, v| 
-    v.each do |inner_key, inner_value|
-      inner_value.each do |names|
-        
-        if !pigeon_name[names]
-          pigeon_name[names] = {}
-        end 
-        
-        if !pigeon_name[names][k]
-          pigeon_name[names][k] = []
-        end 
-        pigeon_name[names][k].push(inner_key).to_s
-        
-      end
-    end 
-  end
-  pigeon_name
+ pigeon_name = {}
+ 
+ data.each do |k, v|
+   v.each do |inner_value, name|
+     k.each do |names|
+       if !pigeon_name[names]
+         pigeon_name[names] = {}
+       end 
+       
+       if !pigeon_name[names][k]
+         pigeon_name[names][k] = [] 
+       end 
+       
+       pigeon_name[names][k].push(inner_value).to_s
+     end 
+   end 
+ end
+ pigeon_name
 end 
